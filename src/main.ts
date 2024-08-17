be953cad33ecb1f7e2455ad9dfc59d972af5d574
Felix Dziatkowski <felix.dziatkowski@googlemail.com>
2024-08-17 18:15:35 +0200

    Update to latest primevue v4 version

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import './assets/main.css';
 /* add all icon we need to the font-awesome library */
 import '@/icons/fontAwesomeIconConfig';
-import 'primevue/resources/themes/aura-dark-amber/theme.css';
-
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Aura from '@primevue/themes/aura';
 import { createPinia } from 'pinia';
 import Button from 'primevue/button';
 import PrimeVue from 'primevue/config';
@@ -15,6 +14,7 @@ import App from './App.vue';
 import router from './router';
 
 import { i18n } from '@/i18n/config';
+import { MainPreset } from '@/PrimeviewPresets/MainPreset';
 
 const app = createApp(App);
 
@@ -25,7 +25,11 @@ app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(createPinia());
 app.use(i18n);
 app.use(router);
-app.use(PrimeVue);
+app.use(PrimeVue, {
+  theme: {
+    preset: MainPreset,
+  }
+});
 app.use(ToastService);
 
 app.mount('#app');
